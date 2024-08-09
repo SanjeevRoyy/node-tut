8519ee8ef2b5cc61830deb3851837e83c8bc151e
SanjeevRoyy <abiralmanausya9@gmail.com>
2024-08-09 15:45:02 +0545

    created routes and dynamic routes

---
+++ script.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  fs.readdir(`./files`, function (err, files) {
+    res.render("index", { files: files });
+  });
+});
+
+app.get("/file/:filename", function (req, res) {
+  fs.readFile(
+    `./files/${req.params.filename}`,
+    "utf-8",
+    function (err, filedata) {
+      res.render("show", { filename: req.params.filename, filedata: filedata });
+    }
+  );
+});
+app.post("/create", function (req, res) {
+  fs.writeFile(
+    `./files/${req.body.title.split("").join("")}.txt`,
+    req.body.details,
+    function (err) {
+      res.redirect("/");
+    }
+  );
+});
+
+app.listen(3000, function () {
+  console.log("server running");
+});
