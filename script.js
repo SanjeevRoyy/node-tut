8ef127511ba6cba7d9731f27de4592cae275262f
SanjeevRoyy <abiralmanausya9@gmail.com>
2024-08-12 14:38:52 +0545

    added edit features

--- script.js
+++ script.js
@@ -34,6 +34,20 @@ app.post("/create", function (req, res) {
   );
 });
 
+app.get("/edit/:filename", function (req, res) {
+  res.render("edit", { filename: req.params.filename });
+});
+
+app.post("/edit", function (req, res) {
+  fs.rename(
+    `./files/${req.body.previous}`,
+    `./files/${req.body.new}`,
+    function (err) {
+      res.redirect("/");
+    }
+  );
+});
+
 app.listen(3000, function () {
   console.log("server running");
 });
